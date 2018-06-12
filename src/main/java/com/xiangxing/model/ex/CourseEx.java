00bd84c55201ae9dc0e96a3ea48643c0f1865d65
s1002h <sun_haocs@163.com>
2018-06-12 18:08:32 +0800

    aaaaaaaaaaa

---
+++ src/main/java/com/xiangxing/model/ex/CourseEx.java
@@ -0,0 +1,16 @@
+package com.xiangxing.model.ex;
+
+import com.xiangxing.model.Course;
+
+public class CourseEx extends Course {
+	private String showCurriculumTime;
+
+	public String getShowCurriculumTime() {
+		return showCurriculumTime;
+	}
+
+	public void setShowCurriculumTime(String showCurriculumTime) {
+		this.showCurriculumTime = showCurriculumTime;
+	}
+
+}
