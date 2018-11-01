f808601c4e0af77aa8ea0c3fb534915675e62d4d
s1002h <sun_haocs@163.com>
2018-11-01 16:14:19 +0800

    qq

--- src/main/java/com/xiangxing/controller/EntryFormController.java
+++ src/main/java/com/xiangxing/controller/EntryFormController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.xiangxing.controller.admin.BaseController;
@@ -117,6 +118,13 @@ public class EntryFormController extends BaseController {
 
 	@RequestMapping("/exportApplyTable")
 	public void exportApplyTable(Long entryFormId, HttpServletResponse response) throws IOException {
+		EntryForm entryForm = entryFormMapper.selectByPrimaryKey(entryFormId);
+		if (entryForm.getPayStatus()==1) {
+			JSONObject jsonObject=new JSONObject();
+			jsonObject.put("info", "未缴费！");
+			writeToErrorResponse(jsonObject);
+			return;
+		}
 		ExamPo examPo = poMapper.get(entryFormId);
 		examPo.setPinyin(HanyuPinyinHelper.toHanyuPinyin(examPo.getStudentName()));
 		String path = pdfPath + File.separator + "考生报名表" + entryFormId + ".pdf";
@@ -148,6 +156,13 @@ public class EntryFormController extends BaseController {
 
 	@RequestMapping("/exportExamTicket")
 	public void exportExamTicket(Long entryFormId, HttpServletResponse response) throws IOException {
+		EntryForm entryForm = entryFormMapper.selectByPrimaryKey(entryFormId);
+		if (entryForm.getPayStatus()==1) {
+			JSONObject jsonObject=new JSONObject();
+			jsonObject.put("info", "未缴费！");
+			writeToErrorResponse(jsonObject);
+			return;
+		}
 		ExamPo examPo = poMapper.get(entryFormId);
 		String path = pdfPath + File.separator + "准考证" + entryFormId + ".pdf";
 		File file = new File(path);
